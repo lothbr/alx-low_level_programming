5f7d1ccf791f5aa96f55954f1829f5ac1aeeedb5
lothbr <abrahamolaoluwa3@gmail.com>
2023-06-15 10:06:11 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all the sie of data types
+ * Return: 0 success
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char:%d byte(s)", sizeof(a));
+printf("Size of an int:%d  byte(s)", sizeof(b));
+printf("Size of a long int:%d byte(s)", sizeof(c));
+printf("Size of a long long int:%d byte(s)", sizeof(d));
+printf("Size of a float:%d byte(s)", sizeof(e));
+return (0);
+}
