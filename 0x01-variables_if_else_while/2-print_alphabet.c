937944c21fa5b0d0d477c6890a8eeb691b3dc552
lothbr <abrahamolaoluwa3@gmail.com>
2023-06-16 09:09:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that print A-Z
+ * Return: 0 to be success
+ */
+int main(void)
+{
+for(char f; f = 'a'; f <= 'z'; f++)
+{
+putchar(f);
+}
+putchar("\n");
+return (0);
+}
