7f19c8b117d797a1c7a858d11bdca60f0e71a2f5
lothbr <abrahamolaoluwa3@gmail.com>
2023-06-17 01:22:16 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program that prints all single digits
+ * with a comma
+ * Return: 0 to be success
+ */
+int main(void)
+{
+int  num;
+for (num = 0; num < 10; num++)
+{
+putchar(('0' + num), ',', ' ');
+}
+putchar('\n');
+return (0);
+}
