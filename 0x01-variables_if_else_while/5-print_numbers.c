6bc68bdfd9622b09d52fd4f0c3da0f49b97f337e
lothbr <abrahamolaoluwa3@gmail.com>
2023-06-17 00:00:14 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all single digits
+ * Return: 0 to be success
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+printf(a);
+}
+printf("\n");
+return (0);
+}
