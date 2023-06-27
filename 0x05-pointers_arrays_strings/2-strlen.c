956d4445f1468f29f5bb89854177162bbdd29e9c
lothbr <abrahamolaoluwa3@gmail.com>
2023-06-27 05:37:19 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strlen - a program that return the len of a string
+ * @s: input string
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+return (strlen(*s));
+}
